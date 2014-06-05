7393f919e076ae4493d23d57d4b7d311b46d4acc
Marilyn <Marilyn@Marilyn-PC>
2014-06-05 13:56:30 -0400

    empezando clase conexion

---
+++ Desktop/libreria_bd/src/libreria_bd/conexion.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreria_bd;
+
+/**
+ *
+ * @author Marilyn
+ */
+public class conexion {
+    
+}
