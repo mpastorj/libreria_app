c39e901b130e32b6315a7031f23152a5d27219cc
Marilyn <Marilyn@Marilyn-PC>
2014-06-24 20:26:30 -0400

    Proyecto Completo

---
+++ Desktop/libreria/Libreria_app/src/libreria_app/Libreria_app.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package libreria_app;
+
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author manolo
+ */
+public class Libreria_app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v=new VentanaPrincipal(); //Creo instancia de la VentanaPrincipal v
+        v.setLocationRelativeTo(null); //Para centrar la Ventana
+        v.setVisible(true); //Para que se vea 
+     
+}
+}
+
+
